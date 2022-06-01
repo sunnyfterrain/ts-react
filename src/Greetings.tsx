3f7deddd8ac7f60006c526dddd4c67a972e0326b
sunnyfterrain <sunnyfterrain@gmail.com>
2022-06-01 23:16:02 +0900

    Add: ts 함수 정의

--- src/Greetings.tsx
+++ src/Greetings.tsx
@@ -4,13 +4,19 @@ type GreetingsProps = {
   name: string;
   mark: string;
   optional?: string; // 있어도 되고? 없어도 되고
+  onClick: (name: string) => void; // 함수일 경우 아무것도 리턴하지 않는다.
 };
 
 // React.FC는 타입을 Generics 사용
-const Greetings = ({ name, mark }: GreetingsProps) => {
+const Greetings = ({ name, mark, optional, onClick }: GreetingsProps) => {
+  const handleClick = () => onClick(name);
   return (
     <div>
       Greetings, {name} {mark}
+      {optional && <p>{optional}</p>}
+      <div>
+        <button onClick={handleClick}>클릭</button>
+      </div>
     </div>
   );
 };
