5c5bb1c9ae4352e86c0559efe0fd088e2d9ce981
sunnyfterrain <sunnyfterrain@gmail.com>
2022-06-06 23:38:41 +0900

    Add: 리덕스모듈

---
+++ src/modules/counter.ts
@@ -0,0 +1,10 @@
+// 뒤에 as const 를 붙여줌으로써 나중에 액션 객체를 만들게 action.type 의 값을 추론하는 과정에서
+// action.type 이 string 으로 추론되지 않고 'counter/INCREASE' 와 같이 실제 문자열 값으로 추론 되도록 해줍니다.
+
+const INCREASE = 'counter/INCREASE' as const;
+const DECREASE = 'counter/DECREASE' as const;
+const INCREASE_BY = 'counter/INCREASE_BY' as const;
+
+export const increase = () => ({
+  type: INCREASE,
+});
