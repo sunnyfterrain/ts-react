efa1f32354cbc2f733e72c23c51ec08a0a99b859
sunnyfterrain <sunnyfterrain@gmail.com>
2022-06-07 16:14:03 +0900

    Add: 타입스크립트 리듀서 추가

--- src/modules/counter.ts
+++ src/modules/counter.ts
@@ -8,3 +8,46 @@ const INCREASE_BY = 'counter/INCREASE_BY' as const;
 export const increase = () => ({
   type: INCREASE,
 });
+
+export const decrease = () => ({
+  type: DECREASE,
+});
+
+export const increaseBy = (diff: number) => ({
+  type: INCREASE_BY,
+  payload: diff,
+});
+
+// 모든 액션 겍체들에 대한 타입을 준비해줍니다.
+// ReturnType<typeof _____> 는 특정 함수의 반환값을 추론
+// 상단부에서 액션타입을 선언 할 떄 as const 를 하지 않으면 이 부분이 제대로 작동하지 않는다.
+type CounterAction =
+  | ReturnType<typeof increase>
+  | ReturnType<typeof decrease>
+  | ReturnType<typeof increaseBy>;
+
+type CounterState = {
+  count: number;
+};
+
+const initialState: CounterState = {
+  count: 0,
+};
+
+// 리듀서 작성
+// 리듀서에서는 state 와 함수의 반환값이 일치하도록 작성
+// 액션에서는 우리가 방금 만든 CounterAction 을 타입으로 설정
+function counter(state: CounterState = initialState, action: CounterAction): CounterState {
+  switch (action.type) {
+    case INCREASE:
+      return { count: state.count + 1 };
+    case DECREASE:
+      return { count: state.count - 1 };
+    case INCREASE_BY:
+      return { count: state.count + action.payload };
+    default:
+      return state;
+  }
+}
+
+export default counter;
