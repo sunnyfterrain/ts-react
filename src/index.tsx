8e1ff76c94d38293e553ced3efab305b0f8030f4
sunnyfterrain <sunnyfterrain@gmail.com>
2022-06-10 14:33:22 +0900

    Add: index 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './modules';
 import './index.css';
 import App from './App';
 
+const store = createStore(rootReducer);
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
